7b95795197f64594e9a1d86b90f5177d56a54b30
Tanguri <jungwontwo2@naver.com>
2024-04-02 10:03:51 +0900

    초기 설정

---
+++ src/test/java/baseball/lostfound/LostfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package baseball.lostfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostfoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
